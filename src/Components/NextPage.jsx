82010fb2b33da538ea5afb768cdaf7f9c706dc2f
davelevine <dave@davelevine.io>
2023-04-26 12:13:36 -0400

    Updated font weights; minor spacing adjustment to navbar

--- src/Components/NextPage.jsx
+++ src/Components/NextPage.jsx
@@ -6,7 +6,7 @@ function NextPage({ currentPage, currentSort }) {
   return (
     <div className="flex flex-row py-4 justify-evenly">
       {(+currentPage) > 1 ? <Link to={`/${currentSort}/${(+currentPage)-1}`}>
-        <div className="flex flex-row items-center px-4 py-2 font-bold text-center rounded-md dark:hover:bg-white hover:bg-black hover:bg-opacity-10 focus-within:outline-none">
+        <div className="flex flex-row items-center px-4 py-2 font-medium text-center rounded-md dark:hover:bg-white hover:bg-black hover:bg-opacity-10 focus-within:outline-none">
           <PreviousIcon size={20}/>
           <div className="ml-2">Previous Page</div>
         </div>
